e2e4445b6bc38cda83ff51e20ce63d2dc50f10ae
youngtyron <gro-jorge@mail.ru>
2018-10-26 15:47:01 +0500

    Add newpro folder again

--- newpro/settings.py
+++ newpro/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'newpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twittbro_db',
+        'USER': 'twittbrouser',
+        'PASSWORD': 'twittbropassword',
+        'HOST':'127.0.0.1',
+        'PORT': 5432,
     }
 }
 
@@ -138,6 +149,8 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
